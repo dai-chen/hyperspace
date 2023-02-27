10ecdf69d1ffd17c6cfb36de860105d05c1bea98
Chen Dai <daichen@amazon.com>
2023-02-27 11:19:40 -0800

    Pass required info to create index command
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- src/main/scala/org/opensearch/maximus/parser/MaximusCreateIndexCommand.scala
+++ src/main/scala/org/opensearch/maximus/parser/MaximusCreateIndexCommand.scala
@@ -1,9 +1,12 @@
 package org.opensearch.maximus.parser
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.execution.command.RunnableCommand
 
 import com.microsoft.hyperspace.Hyperspace
+import com.microsoft.hyperspace.index.dataskipping.DataSkippingIndexConfig
+import com.microsoft.hyperspace.index.dataskipping.sketches.BloomFilterSketch
 
 case class MaximusCreateIndexCommand(
     dbName: Option[String],
@@ -11,12 +14,16 @@ case class MaximusCreateIndexCommand(
     tableName: String,
     columnNames: Seq[String],
     indexProviderName: String)
-  extends RunnableCommand {
-
-  // val hyperspace = new Hyperspace(SparkSession.getActiveSession.get)
+  extends RunnableCommand with Logging {
 
   override def run(sparkSession: SparkSession): Seq[Row] = {
-    // hyperspace.createIndex()
+    log.info(s"Create index $indexName for table $tableName on columns $columnNames")
+
+    val hyperspace = new Hyperspace(sparkSession)
+    val table = sparkSession.sqlContext.table(tableName)
+    val indexConfig = DataSkippingIndexConfig(indexName,
+      BloomFilterSketch(columnNames.head, 0.01, 10))
+    hyperspace.createIndex(table, indexConfig)
     Seq.empty
   }
 }
