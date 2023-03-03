50f804b121486936b957b936e17b20e80fb526a7
Chen Dai <daichen@amazon.com>
2023-03-03 14:36:33 -0800

    Fix show indexes command output
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- src/main/scala/org/opensearch/maximus/command/MaximusShowIndexesCommand.scala
+++ src/main/scala/org/opensearch/maximus/command/MaximusShowIndexesCommand.scala
@@ -1,7 +1,11 @@
 package org.opensearch.maximus.command
 
 import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
 import org.apache.spark.sql.execution.command.RunnableCommand
+import org.apache.spark.sql.types.ArrayType
+import org.apache.spark.sql.types.MapType
+import org.apache.spark.sql.types.StringType
 
 import com.microsoft.hyperspace.Hyperspace
 
@@ -10,10 +14,22 @@ case class MaximusShowIndexesCommand(
     tableName: String)
   extends RunnableCommand {
 
+  override def output: Seq[Attribute] = {
+    Seq(
+      AttributeReference("Name", StringType, nullable = true)(),
+      AttributeReference("indexedColumns",
+                         ArrayType(StringType, true),
+                         nullable = true)(),
+      AttributeReference("indexLocation", StringType, nullable = true)(),
+      AttributeReference("State", StringType, nullable = true)(),
+      AttributeReference("additionalStats",
+                         MapType(StringType, StringType, true),
+                         nullable = true)()
+    )
+  }
+
   override def run(sparkSession: SparkSession): Seq[Row] = {
     val hyperspace = new Hyperspace(sparkSession)
-    val indexes = hyperspace.indexes
-    indexes.show()
-    Seq.empty // TODO: error if return indexes.collect()
+    hyperspace.indexes.collect.toSeq
   }
 }
